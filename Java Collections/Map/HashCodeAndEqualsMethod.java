9fc97f21b72f5dcc8f8d013e9de543e395d12f40
Shubham Ranjan <gkr74226@gmail.com>
2025-08-18 22:34:50 +0530

    HashCodeAndEqualsMethod

--- Java Collections/Map/HashCodeAndEqualsMethod.java
+++ Java Collections/Map/HashCodeAndEqualsMethod.java
@@ -1,6 +1,7 @@
 package Map;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class HashCodeAndEqualsMethod {
     public static void main(String[] args) {
@@ -12,7 +13,12 @@ public class HashCodeAndEqualsMethod {
         map.put(p1,"Engineer");
         map.put(p2,"Manager");
         map.put(p3,"Manager");
-    }
+
+        System.out.println("HashMap Size: " + map.size());
+        System.out.println("Value for p1: " + map.get(p1));
+        // p1 is different from p3 and print Engineer but when we override equal() hashCode() and toString() then it will print same Manager and Manager
+        System.out.println("Value for p3:" + map.get(p3));    // without override it will print Manager
+     }
 }
 
 class Person {
@@ -30,5 +36,30 @@ class Person {
 
     public int getId() {
         return id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Person other = (Person) obj;
+        return id == other.getId() && Objects.equals(name, other.getName());
+    }
+
+    @Override
+    public String toString() {
+        return "id:" +id + ",name:" +name;
     }
 }
