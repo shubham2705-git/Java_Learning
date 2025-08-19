60f7a103f945e4030645de4ff816816217233d8e
Shubham Ranjan <gkr74226@gmail.com>
2025-08-19 10:59:03 +0530

    SortedMap

---
+++ Java Collections/Map/SortedMapDemo.java
@@ -0,0 +1,19 @@
+package Map;
+
+import java.util.*;
+
+public class SortedMapDemo {
+    public static void main(String[] args) {
+        SortedMap<Integer,String> map = new TreeMap<>();
+        map.put(90,"vivek");
+        map.put(99,"shubham");
+        map.put(78,"mohit");
+        map.put(77,"vipul");
+        System.out.println(map);
+
+        System.out.println(map.firstKey());
+        System.out.println(map.lastKey());
+        System.out.println(map.headMap(91));
+        System.out.println(map.tailMap(91));
+    }
+}
