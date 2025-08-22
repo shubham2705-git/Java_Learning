897ea83474adb741e8bb0ef45e39998faf1e3326
Shubham Ranjan <gkr74226@gmail.com>
2025-08-22 22:14:38 +0530

    Predicate

--- Java Collections/Predicate/PredicateDemo.java
+++ Java Collections/Predicate/PredicateDemo.java
@@ -4,7 +4,20 @@ import java.util.function.Predicate;
 
 public class PredicateDemo {
     public static void main(String[] args) {
+
         Predicate<Integer> isEven = x-> x%2==0;
         System.out.println(isEven.test(10));
+
+        Predicate<String> isStartWithA = x->x.toLowerCase().startsWith("a");
+        System.out.println(isStartWithA.test("shubham"));
+
+        Predicate<String> isEndWithA = x->x.toLowerCase().endsWith("a");
+        System.out.println(isEndWithA.test("shubhama"));
+
+        //======================================//
+        Predicate<String> both = isStartWithA.and(isEndWithA);
+        System.out.println(both.test("shubhama"));
+        System.out.println(both.test("AMamaa"));
+
     }
 }
