36a6566ca71afe13f160644a6fcea4deca516c57
Shubham Ranjan <gkr74226@gmail.com>
2025-08-23 22:57:38 +0530

    ExceptionHandling

--- ExceptionHandlingBasics/ExceptionHandlingDemo.java
+++ ExceptionHandlingBasics/ExceptionHandlingDemo.java
@@ -2,6 +2,8 @@ package ExceptionHandlingBasics;
 
 public class ExceptionHandlingDemo {
     public static void main(String[] args) {
-
+        int[] nominators = {10,200,30,40};
+        int[] denominators = {1,2,0,4};
+        System.out.println("hello world");
     }
 }
