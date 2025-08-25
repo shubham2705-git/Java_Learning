fa88bc79542e6ebfa0af355d5fdfdec6c39bdf69
Shubham Ranjan <gkr74226@gmail.com>
2025-08-25 14:55:39 +0530

    ExceptionHandling

--- ExceptionHandlingBasics/ExceptionHandlingDemo.java
+++ ExceptionHandlingBasics/ExceptionHandlingDemo.java
@@ -1,9 +1,22 @@
 package ExceptionHandlingBasics;
 
-public class ExceptionHandlingDemo {
+  class ExceptionHandlingDemo {
     public static void main(String[] args) {
-        int[] nominators = {10,200,30,40};
-        int[] denominators = {1,2,0,4};
-        System.out.println("hello world");
+        int[] numerators = {10, 200, 30, 40};
+        int[] denominators = {1, 2, 0, 4};
+        for (int i = 0; i < numerators.length; i++) {
+            System.out.println(divide(numerators[i], denominators[i]));
+        }
+        System.out.println("good job");
+     }
+        public static int divide(int a, int b)
+        {
+         try {
+             return a / b;
+         }catch (ArithmeticException e)
+         {
+             System.out.println(e);
+             return -1;
+         }
+        }
     }
-}
