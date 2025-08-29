9b1aee9f3698c0c80d1ef5ebe9de1f76fbbe732a
Shubham Ranjan <gkr74226@gmail.com>
2025-08-30 01:07:25 +0530

    ElementAppearOnlyOnce

--- src/DSA/ArrayEasyLevel/FindElementAppearOnce.java
+++ src/DSA/ArrayEasyLevel/FindElementAppearOnce.java
@@ -2,6 +2,7 @@ package ArrayEasyLevel;
 
 public class FindElementAppearOnce {
       public static int elementAppearOnce(int[] arr){
+//=============== Optimal Solution ================
           int xor = 0;
           for(int i : arr){
               xor^=i;
@@ -9,8 +10,32 @@ public class FindElementAppearOnce {
           return xor;
       }
     public static void main(String[] args) {
-        int[] arr = {1,1,2,2,3,4,4};
+        int[] arr = {1,2,2,1,3,5,5,4,4};
         int result = elementAppearOnce(arr);
         System.out.println(result);
     }
 }
+//----------------------------------------------------------------------------
+// ============ BruteForce Solution ============
+/*         for(int i: arr){
+             int  nums = i;
+             int  count = 0;
+           for(int j : arr){
+               if(j == nums)  count++;
+           }
+           if(count == 1) return nums;
+         }
+         return -1;     */
+
+// =========== Better Solution ===============
+/*       HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int num : arr) {
+            if (map.get(num) == 1) {
+                return num;
+            }
+        }
+        return -1; */
