597d2f59e28bdb27820ebe44ad82e657588824a5
Shubham Ranjan <gkr74226@gmail.com>
2025-08-30 21:55:33 +0530

    reverseWordsOfString

---
+++ src/DSA/StringZeroToHero/ReverseWords.java
@@ -0,0 +1,18 @@
+package StringZeroToHero;
+
+public class ReverseWords {
+    public static String reverseWords(String s){
+        String[] words = s.split(" +");
+        StringBuilder reverse = new StringBuilder();
+        for(int i=words.length-1; i>=0; i--){
+            reverse.append(words[i]);
+            reverse.append(" ");
+        }
+        return reverse.toString().trim();
+    }
+    public static void main(String[] args) {
+        String s = "  the sky is blue     " ;
+        String result = reverseWords(s);
+        System.out.println(result);
+    }
+}
