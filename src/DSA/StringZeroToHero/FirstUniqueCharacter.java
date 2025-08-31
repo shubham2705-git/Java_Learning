d2d11e86e45a1857c169d3d197cc7d659926a1da
Shubham Ranjan <gkr74226@gmail.com>
2025-08-31 23:27:39 +0530

    FirstUniqueCharacter

---
+++ src/DSA/StringZeroToHero/FirstUniqueCharacter.java
@@ -0,0 +1,23 @@
+package StringZeroToHero;
+
+public class FirstUniqueCharacter {
+    public static int unique(String s){
+        int[] freq = new int[26];
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            freq[ch-'a']++;
+        }
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(freq[ch-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        String s = "lletvofet";
+        int result = unique(s);
+        System.out.println(result);
+    }
+}
