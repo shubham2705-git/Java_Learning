de7a108b5189d77ba14b07e02fe279a5e7ab095b
Shubham Ranjan <gkr74226@gmail.com>
2025-09-07 16:27:46 +0530

    PowerOfTwo

---
+++ src/DSA/ArrayEasyLevel/PowerOfTwo.java
@@ -0,0 +1,23 @@
+package ArrayEasyLevel;
+
+public class PowerOfTwo {
+    public static boolean isPowerOfTwo(int n) {
+        if(n==0) return false;
+
+        while(n!=1)
+        {
+            if(n%2 != 0)
+            {
+                return false;
+            }
+            else{
+                n=n/2;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int n =17;
+        System.out.println(isPowerOfTwo(n));
+    }
+}
