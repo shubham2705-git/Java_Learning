b9428adf5e4ff64efdb216d27426fbef94af3f0e
Shubham Ranjan <gkr74226@gmail.com>
2025-09-12 23:57:21 +0530

    ValidParenthesis

--- src/DSA/StringZeroToHero/ValidParenthesis.java
+++ src/DSA/StringZeroToHero/ValidParenthesis.java
@@ -3,7 +3,7 @@ package StringZeroToHero;
 public class ValidParenthesis {
     public static void main(String[] args) {
         String s = "()";
-
-
+        boolean result = validParen(s);
+        System.out.println(result);
     }
 }
