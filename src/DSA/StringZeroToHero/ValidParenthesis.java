03f29d077fb2b9eb784aecbf0a3eb885b9e34c3d
Shubham Ranjan <gkr74226@gmail.com>
2025-09-12 23:59:41 +0530

    ValidParenthesis

--- src/DSA/StringZeroToHero/ValidParenthesis.java
+++ src/DSA/StringZeroToHero/ValidParenthesis.java
@@ -6,6 +6,9 @@ public class ValidParenthesis {
             if(s.contains("()")){
                 s=s.replace("()","");
             }
+            else if(s.contains("{}")){
+                s=s.replace("{}","")
+            }
 
         }
 
