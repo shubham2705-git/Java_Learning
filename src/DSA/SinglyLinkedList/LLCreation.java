0d5dd0edd2830e5c64aa5e04d6a27a782dd91d27
Shubham Ranjan <gkr74226@gmail.com>
2025-09-17 21:27:16 +0530

    LearningLinkedList

--- src/DSA/SinglyLinkedList/LLCreation.java
+++ src/DSA/SinglyLinkedList/LLCreation.java
@@ -14,9 +14,34 @@ class Node{
     }
 }
 public class LLCreation{
+    private static Node convertArr2LL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i=1; i<arr.length; i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+    public static int lengthOfLL(Node head){
+        int cnt = 0;
+        Node temp = head;
+        while(temp!=null){
+            System.out.println(temp.data+" ");
+            temp = temp.next;
+            cnt++;
+        }
+        return cnt;
+    }
     public static void main(String[] args) {
         int[] arr = {2,5,6,8};
-        Node head = new Node(arr[2]);
-        System.out.println(head.data);
+        Node head = convertArr2LL(arr);
+//        Node temp = head;
+//        while(temp!=null){
+//            System.out.println(temp.data+" ");
+//            temp = temp.next;
+//        }
+        System.out.println("length of LL is: "+ lengthOfLL(head));
     }
 }
