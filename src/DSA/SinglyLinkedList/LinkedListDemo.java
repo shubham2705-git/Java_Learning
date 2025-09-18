a10f9d86a605ec5d8e6e213e827ad9fff003e89a
Shubham Ranjan <gkr74226@gmail.com>
2025-09-18 22:46:30 +0530

    findMiddle

---
+++ src/DSA/SinglyLinkedList/LinkedListDemo.java
@@ -0,0 +1,77 @@
+package SinglyLinkedList;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data1,Node next1){
+        this.data=data1;
+        this.next=next1;
+    }
+    Node(int data1){
+        this.data=data1;
+        this.next=null;
+    }
+}
+
+public class LinkedListDemo{
+
+        private static Node convertArr2LL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i=1; i<arr.length; i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+    public static Node findMiddle(Node head){
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,4,5,6,8};
+        Node head = convertArr2LL(arr);
+        Node middleNode = findMiddle(head);
+        System.out.println("middle Node is:" + middleNode.data);
+    }
+}
+
+
+// ==============     CONVERTING ARRAY TO LL   =========================================================================
+//    private static Node convertArr2LL(int[] arr){
+//        Node head = new Node(arr[0]);
+//        Node mover = head;
+//        for(int i=1; i<arr.length; i++){
+//            Node temp = new Node(arr[i]);
+//            mover.next = temp;
+//            mover = temp;
+//        }
+//        return head;
+//    }
+// ==================    FINDING LENGTH OF LL    ====================================================================
+//    public static int lengthOfLL(Node head){
+//        int cnt = 0;
+//        Node temp = head;
+//        while(temp!=null){
+//            System.out.println(temp.data+" ");
+//            temp = temp.next;
+//            cnt++;
+//        }
+//        return cnt;
+//    }
+// ===================     CHECK IF PRESENT    =====================================================================
+//    public static boolean checkIfPresent(Node head, int val){
+//        Node temp = head;
+//        while(temp!=null){
+//            if(temp.data == val) return true;
+//            temp = temp.next;
+//        }
+//        return  false;
+//    }
