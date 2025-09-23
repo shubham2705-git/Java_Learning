efc52cdd800b2294516bc66dd8975b856131d436
Shubham Ranjan <gkr74226@gmail.com>
2025-09-23 21:13:19 +0530

    SetMatrixZeroes

--- src/DSA/ArrayMediumLevel/SetMatrixZeroes.java
+++ src/DSA/ArrayMediumLevel/SetMatrixZeroes.java
@@ -3,7 +3,7 @@ package ArrayMediumLevel;
 import java.util.Arrays;
 
 public class SetMatrixZeroes {
-    int setMatrixZeroes(int[][] arr){
+    public static int[][] setMatrixZeroes(int[][] arr) {
         int n = arr.length;
         int m = arr[0].length;
         int[] row = new int[n];
@@ -11,10 +11,24 @@ public class SetMatrixZeroes {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (arr[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    arr[i][j] = 0 ;
+                }
+            }
+        }
+        return arr;
     }
+
     public static void main(String[] args) {
         int[][] arr= {{1,1,1,1},{1,0,1,1},{1,1,0,1},{0,1,1,0}};
         int[][] result = setMatrixZeroes(arr);
-        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.deepToString(result));
     }
 }
