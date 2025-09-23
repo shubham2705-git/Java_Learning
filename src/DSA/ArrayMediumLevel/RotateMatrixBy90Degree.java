5d741e8d7ab12b249fd36ac8ae6a481c6a070a82
Shubham Ranjan <gkr74226@gmail.com>
2025-09-23 23:15:53 +0530

    RotateMatrixBy90Degree

--- src/DSA/ArrayMediumLevel/RotateMatrixBy90Degree.java
+++ src/DSA/ArrayMediumLevel/RotateMatrixBy90Degree.java
@@ -15,7 +15,7 @@ public class RotateMatrixBy90Degree {
     }
     public static void main(String[] args) {
         int[][] matrix =  {{1,2,3},{4,5,6},{7,8,9}};
-        int ans[][] = rotateImage(matrix);
+        int[][] ans = rotateImage(matrix);
         System.out.println(Arrays.deepToString(ans));
     }
 }
