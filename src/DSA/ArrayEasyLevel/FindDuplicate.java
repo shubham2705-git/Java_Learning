998bb40cc0b6e92888dd80abde4344bf7fe906cf
Shubham Ranjan <gkr74226@gmail.com>
2025-09-25 19:42:49 +0530

    findDuplicate

--- src/DSA/ArrayEasyLevel/FindDuplicate.java
+++ src/DSA/ArrayEasyLevel/FindDuplicate.java
@@ -6,8 +6,8 @@ public class FindDuplicate {
     public static int[] findDuplicate(int[] arr){
         Map<Integer,Integer> map = new HashMap<>();
         List<Integer> list = new ArrayList<>();
-        for(int i=0;i<arr.length;i++){
-            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        for(int i:arr){
+            map.put(i,map.getOrDefault(i,0)+1);
         }
         for(Map.Entry<Integer,Integer> e:map.entrySet()){
             if(e.getValue()>1){
