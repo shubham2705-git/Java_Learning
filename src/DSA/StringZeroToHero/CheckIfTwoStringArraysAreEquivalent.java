279b2369697841f3dc46ebc4ff7a8a0a2af24215
Shubham Ranjan <gkr74226@gmail.com>
2025-10-04 01:03:23 +0530

    CheckIfTwoStringArraysAreEquivalent..LC-1662

--- src/DSA/StringZeroToHero/CheckIfTwoStringArraysAreEquivalent.java
+++ src/DSA/StringZeroToHero/CheckIfTwoStringArraysAreEquivalent.java
@@ -2,15 +2,17 @@ package StringZeroToHero;
 
 public class CheckIfTwoStringArraysAreEquivalent {
     public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-            StringBuilder result1 = new StringBuilder();
-            for(String str: word1){
-                result1.append(str);
-            }
-            StringBuilder result2 = new StringBuilder();
-            for(String str: word2){
-                result2.append(str);
-            }
-            return result1.equals(result2);
+        StringBuilder result1 = new StringBuilder();
+        for(String str: word1){
+            result1.append(str);
+        }
+        String str1 = result1.toString();
+        StringBuilder result2 = new StringBuilder();
+        for(String str: word2){
+            result2.append(str);
+        }
+        String str2 = result2.toString();
+        return str1.equals(str2);
         }
     public static void main(String[] args) {
       String[] word1 = {"a","bc"};
