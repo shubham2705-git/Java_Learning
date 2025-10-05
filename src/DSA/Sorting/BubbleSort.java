dc605b0a0a934ce4655579f1e782ab55e39c84b9
Shubham Ranjan <gkr74226@gmail.com>
2025-10-05 15:50:45 +0530

    BubbleSort

--- src/DSA/Sorting/BubbleSort.java
+++ src/DSA/Sorting/BubbleSort.java
@@ -5,13 +5,16 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void bubbleSort(int[] arr){
         for(int i=arr.length-1;i>=1;i--){
+            int didSwap = 0;
             for(int j=0;j<i;j++){
                 if(arr[j]>arr[j+1]){
                     int temp =arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    didSwap = 1;
                 }
             }
+            if(didSwap==0) break;
         }
         System.out.println(Arrays.toString(arr));
     }
