65d00cd04784e8a783256d9294759fac0bec13b3
Shubham Ranjan <gkr74226@gmail.com>
2025-10-06 14:51:18 +0530

    findDuplicate

--- src/DSA/ArrayEasyLevel/FindDuplicate.java
+++ src/DSA/ArrayEasyLevel/FindDuplicate.java
@@ -4,7 +4,19 @@ import java.util.*;
 
 public class FindDuplicate {
     public static int[] findDuplicate(int[] arr){
-        Map<Integer,Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        for(int i:arr){
+            if(set.contains(i)){
+                System.out.print(i+" ");
+            }else{
+                set.add(i);
+            }
+        }
+        System.out.println();
+        return set.stream().mapToInt(Integer::intValue).toArray();
+
+
+        /*Map<Integer,Integer> map = new HashMap<>();
         List<Integer> list = new ArrayList<>();
         for(int i:arr){
             map.put(i,map.getOrDefault(i,0)+1);
@@ -14,11 +26,12 @@ public class FindDuplicate {
                 list.add(e.getKey());
             }
         }
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        return list.stream().mapToInt(Integer::intValue).toArray();*/
     }
     public static void main(String[] args) {
         int[] arr={1,5,4,2,1,4,7};
+        System.out.print("the duplicate elements are: ");
         int[] result = findDuplicate(arr);
-        System.out.println(Arrays.toString(result));
+        System.out.println("After removing duplicates the array becomes "+Arrays.toString(result));
     }
 }
