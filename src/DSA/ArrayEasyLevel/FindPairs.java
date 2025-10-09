d98b461d1f83799503f7a512e19524dca1bf98ce
Shubham Ranjan <gkr74226@gmail.com>
2025-10-09 20:08:32 +0530

    FindPairs

--- src/DSA/ArrayEasyLevel/FindPairs.java
+++ src/DSA/ArrayEasyLevel/FindPairs.java
@@ -1,19 +1,20 @@
 package ArrayEasyLevel;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class FindPairs {
     public static void findPairs(int[] arr,int target){
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            for(int j=1;j<arr.length;j++){
-              if(arr[i]+arr[j]==target){
-                  map.put(arr[i],arr[j]);
-              }
+        int low = 0;
+        int high = arr.length-1;
+        while(low<high){
+            if(arr[low]+arr[high]>target){
+                high--;
+            }else if(arr[low]+arr[high]<target){
+                low++;
+            }else{
+                System.out.println(arr[low]+ " "+arr[high]);
+                high--;
+                low++;
             }
         }
-            System.out.println(map);
     }
     public static void main(String[] args) {
         int[] arr= {1,2,3,4,5,6,7};
