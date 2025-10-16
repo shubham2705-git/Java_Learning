d2fe24b18f037046adee13f16d05b9960e335a95
Shubham Ranjan <gkr74226@gmail.com>
2025-10-16 13:37:09 +0530

    Factorial

--- src/DSA/recursion/Factorial.java
+++ src/DSA/recursion/Factorial.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class Factorial {
     public static int factorial(int n){
         if(n==0) return 1;
-
-        int subFact = factorial(n-1);
-        return n*subFact;
+        return n*factorial(n-1);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
