1d4ed6c0d015f0d69df74a903557f8e7809cff93
Shubham Ranjan <gkr74226@gmail.com>
2025-10-19 21:19:52 +0530

    LowerBound

---
+++ src/DSA/BinarySearch/LowerBound.java
@@ -0,0 +1,24 @@
+package BinarySearch;
+
+public class LowerBound {
+    public static int lowerBound(int[] arr,int target){
+        int low = 0;
+        int ans = arr.length;
+        int high = arr.length-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+           if(arr[mid]>=target){
+               ans=mid;
+               high=mid-1;
+           }else{
+               low = mid+1;
+           }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,5,6,7,12,15,23};
+        int target = 13;
+        System.out.println(lowerBound(arr,target));
+    }
+}
