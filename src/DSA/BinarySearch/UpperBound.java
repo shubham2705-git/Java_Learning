f3cf9c0da3155318d61e9218a0bda0fb7946dcf0
Shubham Ranjan <gkr74226@gmail.com>
2025-10-23 15:07:37 +0530

    UpperBound

--- src/DSA/BinarySearch/UpperBound.java
+++ src/DSA/BinarySearch/UpperBound.java
@@ -1,7 +1,7 @@
 package BinarySearch;
 
 public class UpperBound {
-    public static int lowerBound(int[] arr,int target){
+    public static int upperBound(int[] arr,int target){
         int low = 0;
         int ans = arr.length;
         int high = arr.length-1;
@@ -15,11 +15,10 @@ public class UpperBound {
             }
         }
         return ans;
-
     }
     public static void main(String[] args) {
         int[] arr = {2,3,4,5,6,7,12,15,23};
-        int target = 22;
-        System.out.println(lowerBound(arr,target));
+        int target = 12;
+        System.out.println(upperBound(arr,target));
     }
 }
