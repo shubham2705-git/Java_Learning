0764ce9fb8a708cc6eaaed735978451edcabb340
Shubham Ranjan <gkr74226@gmail.com>
2025-10-29 22:19:35 +0530

    FirstAndLastOccurance..Using BS

--- src/DSA/BinarySearch/FirstAndLastOccurance.java
+++ src/DSA/BinarySearch/FirstAndLastOccurance.java
@@ -1,5 +1,6 @@
 package BinarySearch;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class FirstAndLastOccurance {
@@ -48,3 +49,61 @@ public class FirstAndLastOccurance {
         System.out.println(Arrays.toString(result));
     }
 }
+
+
+// ============ Optimal approach ===================
+// User function Template for Java
+
+//class FirstAndLastOccurance {
+//    public static int first(int[] arr,int target){
+//        int n = arr.length;
+//        int low = 0;
+//        int high = n-1;
+//        int first = -1;
+//        while(low<=high){
+//            int mid = low + (high - low)/2;
+//            if(arr[mid]==target){
+//                first = mid;
+//                high = mid-1;
+//            }
+//            else if(arr[mid]<target){
+//                low = mid+1;
+//            }
+//            else{
+//                high = mid-1;
+//            }
+//        }
+//        return first;
+//    }
+//    public static int last(int[] arr,int target){
+//        int n = arr.length;
+//        int low = 0;
+//        int high = n-1;
+//        int last = -1;
+//        while(low<=high){
+//            int mid = low + (high - low)/2;
+//            if(arr[mid]==target){
+//                last = mid;
+//                low = mid+1;
+//            }
+//            else if(arr[mid]<target){
+//                low = mid+1;
+//            }
+//            else{
+//                high = mid-1;
+//            }
+//        }
+//        return last;
+//    }
+//    ArrayList<Integer> find(int arr[], int x) {
+//        int[] arr = {2,4,5,5,5,5,5,7,8,11,11,11,11,14,15};
+//        int x = 5;
+//        int first = first(arr,x);
+//        int last = last(arr,x);
+//        int[] arr = {2,4,5,7,8,11,14,15};
+//        ArrayList<Integer> list = new ArrayList<>();
+//        list.add(first);
+//        list.add(last);
+//        return list;
+//    }
+//}
