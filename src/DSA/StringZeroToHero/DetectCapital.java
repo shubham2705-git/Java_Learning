ce6d28cc5f4fd6be6015732ecb5eabaa955462dc
Shubham Ranjan <gkr74226@gmail.com>
2025-11-01 23:55:11 +0530

    DetectCapital..Lc-520

---
+++ src/DSA/StringZeroToHero/DetectCapital.java
@@ -0,0 +1,20 @@
+package StringZeroToHero;
+
+public class DetectCapital {
+    public static boolean detectCapitalUse(String word) {
+        int count = 0;
+    for(int i=0; i<word.length();i++){
+        if(word.charAt(i)>='A' && word.charAt(i)<='Z'){
+            count++;
+        }
+    }
+    if(count==word.length()) return true;
+    if(count==0) return true;
+     return count == 1 && word.charAt(0) >= 'A' && word.charAt(0) <= 'Z';
+    }
+    public static void main(String[] args) {
+        String str = "Leetcode";
+        boolean result = detectCapitalUse(str);
+        System.out.println(result);
+    }
+}
