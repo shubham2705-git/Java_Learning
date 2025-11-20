7a188182e1a85caee9641b6385bcbe0a3e32263f
Shubham Ranjan <gkr74226@gmail.com>
2025-11-21 00:11:48 +0530

    GroupAnagrams..LC-459

---
+++ src/DSA/StringZeroToHero/RepeatedSubstringPattern.java
@@ -0,0 +1,25 @@
+package StringZeroToHero;
+
+public class RepeatedSubstringPattern {
+        public static boolean repeatedSubstringPattern(String s) {
+            for(int i=s.length()/2;i>0;i--){
+                if(s.length()%i==0){
+                    int times=s.length()/i;
+                    String pattern = s.substring(0,i);
+                    String newStr = "";
+                    while(times!=0){
+                        newStr+=pattern;
+                        times--;
+                    }
+                    if(newStr.equals(s)){
+                        return true;
+                    }
+                }
+            }
+            return false;
+    }
+    public static void main(String[] args) {
+        String str="abcabcabcabc";
+        System.out.println(repeatedSubstringPattern(str));
+    }
+}
