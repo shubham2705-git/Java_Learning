a12e45b8172b2ac625256b3808099122802e0b3b
Shubham Ranjan <gkr74226@gmail.com>
2025-12-13 23:51:46 +0530

    UniqueCharacters

---
+++ src/DSA/StringZeroToHero/Conizant1.java
@@ -0,0 +1,33 @@
+package StringZeroToHero;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Conizant1 {
+    public static int solution(int k, String s1, String s2){
+        Set<String> uniqueSubstrings = new HashSet<>();
+        int count = 0;
+
+        for (int i = 0; i <= s1.length() - k; i++) {
+            String sub = s1.substring(i, i + k);
+            uniqueSubstrings.add(sub);
+        }
+        for (String sub : uniqueSubstrings) {
+            if (s2.contains(sub)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int k = sc.nextInt();
+        sc.nextLine();
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        int result = solution(k,s1,s2);
+        System.out.println(result);
+
+    }
+}
