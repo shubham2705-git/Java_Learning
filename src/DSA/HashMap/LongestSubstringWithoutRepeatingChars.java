087a2b09546c957f1b977d947f78a944df221cf6
Shubham Ranjan <gkr74226@gmail.com>
2025-12-19 23:29:15 +0530

    LongestSubstringWithoutRepeatingChars..Using array

--- src/DSA/HashMap/LongestSubstringWithoutRepeatingChars.java
+++ src/DSA/HashMap/LongestSubstringWithoutRepeatingChars.java
@@ -1,5 +1,6 @@
 package HashMap;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,14 +19,31 @@ public class LongestSubstringWithoutRepeatingChars {
 //            }
 //        }
 //        return maxLen;
-        int max=0;
-        for(int i=0;i<s.length();i++){
-            Set<Character> set = new HashSet<>();
-            for(int j=i;j<s.length();j++){
-                if(set.contains(s.charAt(j))) break;
-                set.add(s.charAt(j));
-                max=Math.max(max,set.size());
+
+
+//        int max=0;
+//        for(int i=0;i<s.length();i++){
+//            Set<Character> set = new HashSet<>();
+//            for(int j=i;j<s.length();j++){
+//                if(set.contains(s.charAt(j))) break;
+//                set.add(s.charAt(j));
+//                max=Math.max(max,set.size());
+//            }
+//        }
+//        return max;
+
+        int[] arr = new int[256];
+        Arrays.fill(arr,-1);
+        int l=0,r=0,max=0;
+        while(r<s.length()){
+            char ch = s.charAt(r);
+            if(arr[ch]!=-1){
+                l=Math.max(arr[ch]+1,l);
             }
+            int len = r-l+1;
+            max=Math.max(max,len);
+            arr[ch]=r;
+            r++;
         }
         return max;
     }
