f06421bcb1ef2cd991c1471f25d30dac10950cdd
Shubham Ranjan <gkr74226@gmail.com>
2025-12-19 20:47:56 +0530

    LongestSubstringWithoutRepeatingChars..Using List

--- src/DSA/HashMap/LongestSubstringWithoutRepeatingChars.java
+++ src/DSA/HashMap/LongestSubstringWithoutRepeatingChars.java
@@ -1,20 +1,33 @@
 package HashMap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LongestSubstringWithoutRepeatingChars {
     public static int result(String s){
-        int maxLen=0;
+//        int maxLen=0;
+//        for(int i=0;i<s.length();i++){
+//            int[] arr = new int[256];
+//            int len = 0;
+//            for(int j=i;j<s.length();j++){
+//                char ch = s.charAt(j);
+//                if(arr[ch]==1) break;
+//                len=j-i+1;
+//                maxLen=Math.max(len,maxLen);
+//                arr[ch-'a']=1;
+//            }
+//        }
+//        return maxLen;
+        int max=0;
         for(int i=0;i<s.length();i++){
-            int[] arr = new int[256];
-            int len = 0;
+            List<Character> list = new ArrayList<>();
             for(int j=i;j<s.length();j++){
-                char ch = s.charAt(j);
-                if(arr[ch]==1) break;
-                len=j-i+1;
-                maxLen=Math.max(len,maxLen);
-                arr[ch-'a']=1;
+                if(list.contains(s.charAt(j))) break;
+                list.add(s.charAt(j));
+                max=Math.max(max,list.size());
             }
         }
-        return maxLen;
+        return max;
     }
     public static void main(String[] args) {
         String s = "cadbzabcd";
