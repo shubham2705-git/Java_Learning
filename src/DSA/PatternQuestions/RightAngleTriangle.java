10ef95bfd7ffd57377d661b514b63f577d959d7e
Shubham Ranjan <gkr74226@gmail.com>
2025-12-22 13:52:54 +0530

    RightAngleTriangle

--- src/DSA/PatternQuestions/RightAngleTriangle.java
+++ src/DSA/PatternQuestions/RightAngleTriangle.java
@@ -2,14 +2,15 @@ package PatternQuestions;
 
 public class RightAngleTriangle {
     public static void print(int n){
+        int count=1;
         for(int i=0;i<n;i++){
             for(int j=0;j<i+1;j++){
-                System.out.print("* ");
+                System.out.print(count++ +" ");
             }
             System.out.println();
         }
     }
     public static void main(String[] args) {
-        print(10);
+        print(5);
     }
 }
