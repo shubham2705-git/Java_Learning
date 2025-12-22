2944d039cc7aa3f1d455656188075f965d950d2f
Shubham Ranjan <gkr74226@gmail.com>
2025-12-22 13:53:35 +0530

    Cognizant2

---
+++ src/DSA/StringZeroToHero/Cognizant2.java
@@ -0,0 +1,29 @@
+package StringZeroToHero;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cognizant2 {
+    public static String print(String s){
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(!map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),i+1);
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            int count=map.get(s.charAt(i));
+            while(count!=0){
+                ans.append(s.charAt(i));
+                count--;
+            }
+            ans.append("-");
+        }
+        return ans.toString();
+    }
+    public static void main(String[] args) {
+        String s = "abcaba";
+        System.out.println(print(s));
+    }
+}
