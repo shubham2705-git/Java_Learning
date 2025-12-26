8bf9d6c358a7642e9f0204ee065639b5c78d407d
Shubham Ranjan <gkr74226@gmail.com>
2025-12-26 23:14:31 +0530

    LinkedList Operations..

---
+++ src/DSA/SinglyLinkedList/CreateLinkedList.java
@@ -0,0 +1,87 @@
+package SinglyLinkedList;
+
+public class CreateLinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static void printList(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+    static Node deleteHead(Node head) {
+        if (head == null) return null;
+        return head.next;
+    }
+
+    static Node deleteTail(Node head) {
+        if (head == null || head.next==null) return null;
+        Node temp = head;
+        while(temp.next.next!=null){
+            temp=temp.next;
+        }
+        temp.next=null;
+        return head;
+    }
+
+    static Node deleteKthElement(Node head,int k){
+        if(head==null) return head;
+        if(k==1){
+            return head.next;
+        }
+        Node temp = head;
+        int count=0;
+        Node prev = null;
+        while(temp!=null){
+            count++;
+            if(count==k){
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+
+
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        Node temp = head;
+
+        for(int i=2; i<=7; i++){
+            temp.next = new Node(i);
+            temp = temp.next;
+        }
+        System.out.println("Before deleting head:");
+        printList(head);
+        System.out.println("------------------------------------");
+
+        Node first = deleteHead(head);
+        System.out.println("After deleting head:");
+        printList(first);
+        System.out.println("------------------------------------");
+
+        Node second=deleteTail(head);
+        System.out.println("After deleting tail");
+        printList(second);
+        System.out.println("------------------------------------");
+
+        Node third = deleteKthElement(head,3);
+        System.out.println("After deleting kth Element");
+        printList(third);
+        System.out.println("------------------------------------");
+    }
+
+}
