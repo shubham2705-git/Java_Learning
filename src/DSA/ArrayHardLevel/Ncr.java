412e7fba97e8e90e33ae04bc723df591702277ed
Shubham Ranjan <gkr74226@gmail.com>
2026-02-03 17:26:33 +0530

    pascal's triangle ..

---
+++ src/DSA/ArrayHardLevel/Ncr.java
@@ -0,0 +1,27 @@
+package ArrayHardLevel;
+
+public class Ncr {
+    // find the value in pascal triangle at give index
+    public static int findNcr(int n,int r){
+        int res = 1;
+        for(int i=0;i<r;i++)
+        {
+            res = res * (n-i);
+            res = res / (i+1);
+        }
+        return res;
+    }
+    // print the given row in pascal triangle
+    public static void printRow(int n){
+        for(int i=1;i<=n;i++){
+            System.out.print(findNcr(n-1,i-1)+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        int r = 3;
+        int result = findNcr(n,r);
+        System.out.println(result);
+        printRow(n);
+    }
+}
