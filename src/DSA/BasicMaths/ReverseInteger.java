20bd0c9f20337a1e2f7cd2ffbd15a72b0143cc3e
Shubham Ranjan <gkr74226@gmail.com>
2026-02-07 15:22:25 +0530

    reverse integer ..

---
+++ src/DSA/BasicMaths/ReverseInteger.java
@@ -0,0 +1,9 @@
+package BasicMaths;
+
+class ReverseInteger{
+    public static void main(String args[]){
+        int num=1234566006;
+        String s = num+"";
+        System.out.println(s);
+        }}
+
