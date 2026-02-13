49137582d2f5ebdb2f5c045ae7ca99ac4904047b
Shubham Ranjan <gkr74226@gmail.com>
2026-02-14 02:22:17 +0530

    FindFactors ..

---
+++ src/DSA/BasicMaths/FindFactors.java
@@ -0,0 +1,15 @@
+package BasicMaths;
+
+public class FindFactors {
+    public static void main(String[] args) {
+        int n = 12;
+        for(int i=1;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                System.out.print(i+" ");
+            }
+            if(n/i!=i){
+                System.out.print(n/i+" ");
+            }
+        }
+    }
+}
