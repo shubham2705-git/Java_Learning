721fd4e9fb7509175b29dd3f0bb4df94b15182cd
Shubham Ranjan <gkr74226@gmail.com>
2026-02-14 02:30:04 +0530

    FIndGCD ..

--- src/DSA/BasicMaths/FIndGCD.java
+++ src/DSA/BasicMaths/FIndGCD.java
@@ -2,15 +2,14 @@ package BasicMaths;
 
 public class FIndGCD {
     public static void main(String[] args) {
-        int n = 12;
-        int m = 9;
+        int n = 20;
+        int m = 30;
         int j = Math.min(n,m);
-        int gcd = 1;
-        for(int i=1;i<j;i++){
-            if(n%i==0 && m%i==0){
-                gcd = i;
+        for(int i=j;i>=1;i--) {
+            if (n % i == 0 && m % i == 0) {
+                System.out.println(i);
+                break;
             }
         }
-        System.out.println(gcd);
     }
 }
