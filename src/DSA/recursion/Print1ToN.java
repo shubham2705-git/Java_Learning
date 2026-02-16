f785e51b1d65666571737a4b713e8eb5424f0a1e
Shubham Ranjan <gkr74226@gmail.com>
2026-02-16 19:09:26 +0530

    print 1 to n..

--- src/DSA/recursion/Print1ToN.java
+++ src/DSA/recursion/Print1ToN.java
@@ -1,12 +1,14 @@
 package recursion;
 
 public class Print1ToN {
-    public static void print(int n){
-        if(n==0)  return;
-        System.out.println(n);
-        print(n-1);
+    public static void print(int count){
+        if(count>0) {
+            print(count - 1);
+            System.out.println(count);
+        }
     }
     public static void main(String[] args) {
-        print(5);
+        int count = 5;
+        print(count);
     }
 }
