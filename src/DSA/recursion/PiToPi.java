f57b8686c3c86d20346b8ecb295e19eecd96ef0d
Shubham Ranjan <gkr74226@gmail.com>
2026-02-16 19:18:49 +0530

    pi to 3.14..

---
+++ src/DSA/recursion/PiToPi.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class PiToPi {
+    public static String pi(String str){
+        if(str.length()<=1){
+            return str;
+        }
+        if(str.charAt(0)=='p' && str.charAt(1)=='i'){
+            return "3.14"+pi(str.substring(2));
+        }
+        return str.charAt(0)+pi(str.substring(1));
+    }
+    public static void main(String[] args) {
+        String str = "23kpikkpi";
+        System.out.println(pi(str));
+    }
+}
