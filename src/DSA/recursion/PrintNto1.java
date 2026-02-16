17615f807ca2c01a9af66f9278b4c17a072ba80c
Shubham Ranjan <gkr74226@gmail.com>
2026-02-16 19:09:39 +0530

    print n to 1..

---
+++ src/DSA/recursion/PrintNto1.java
@@ -0,0 +1,12 @@
+package recursion;
+
+public class PrintNto1 {
+    public static void print(int n){
+        if(n==0)  return;
+        System.out.println(n);
+        print(n-1);
+    }
+    public static void main(String[] args) {
+        print(5);
+    }
+}
