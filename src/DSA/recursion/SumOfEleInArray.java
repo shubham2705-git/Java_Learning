7ad8f8dbfdf70379276d435973d1c4273fbc82d0
Shubham Ranjan <gkr74226@gmail.com>
2026-02-16 15:28:39 +0530

    Sum Of Elements of Array ..

--- src/DSA/recursion/SumOfEleInArray.java
+++ src/DSA/recursion/SumOfEleInArray.java
@@ -2,7 +2,7 @@ package recursion;
 
 public class SumOfEleInArray {
     public static int sumOfEle(int[] arr, int i){
-        if(i==arr.length) return 0;
+        if(i==arr.length)  return 0;
         return arr[i]+sumOfEle(arr,i+1);
     }
     public static void main(String[] args) {
