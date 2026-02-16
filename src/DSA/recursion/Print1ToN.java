d8d97536806d58b09bab5581006dfc017c45cdc9
Shubham Ranjan <gkr74226@gmail.com>
2026-02-16 14:11:09 +0530

    commit ..

---
+++ src/DSA/recursion/Print1ToN.java
@@ -0,0 +1,12 @@
+package recursion;
+
+public class Print1ToN {
+    public static void print(int n){
+        if(n==0) return;
+        print(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args) {
+        print(5);
+    }
+}
