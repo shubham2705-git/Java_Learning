0953edc264dd2a1d1b7bf603a3e61e21cd6a575d
Shubham Ranjan <gkr74226@gmail.com>
2026-02-16 14:54:51 +0530

    print1ToN ..

--- src/DSA/recursion/Print1ToN.java
+++ src/DSA/recursion/Print1ToN.java
@@ -3,8 +3,8 @@ package recursion;
 public class Print1ToN {
     public static void print(int n){
         if(n==0) return;
-        print(n-1);
         System.out.println(n);
+        print(n-1);
     }
     public static void main(String[] args) {
         print(5);
