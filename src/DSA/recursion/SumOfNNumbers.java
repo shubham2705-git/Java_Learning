587f77fd7f29c360cb3c03f7d7579a605aa8b7f7
Shubham Ranjan <gkr74226@gmail.com>
2026-02-16 14:23:30 +0530

    SumOfNNumbers ..

--- src/DSA/recursion/SumOfNNumbers.java
+++ src/DSA/recursion/SumOfNNumbers.java
@@ -3,17 +3,17 @@ package recursion;
 import java.util.Scanner;
 
 public class SumOfNNumbers {
-    public static void print(int i,int sum){
-        if(i<0) {
-            System.out.println(sum);
-            return;
-        }
-        print(i-1, sum+i);
+    public static int print(int n){
+        int x;
+        if(n<=1) return n;
+        x = print(n-1)+n;
+        return x;
+
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter any number: ");
         int n = sc.nextInt();
-        print(n,0);
+        System.out.println(print(n));
     }
 }
