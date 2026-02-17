1379609eb21efd587f0968cc4455a1e1ce4aed19
Shubham Ranjan <gkr74226@gmail.com>
2026-02-17 10:32:24 +0530

    Check Palindrome String..

--- src/DSA/recursion/CheckPalindrome.java
+++ src/DSA/recursion/CheckPalindrome.java
@@ -1,15 +1,15 @@
 package recursion;
 
 public class CheckPalindrome {
-    public static boolean checkPalindrome(String str, int i){
-        if(i>=str.length()/2) return true;
-        if(str.charAt(i)!=str.charAt(str.length()-i-1)) return false;
-        return checkPalindrome(str,i+1);
+    public static boolean checkPalindrome(String str){
+        if(str.length()<=1) return true;
+        if(str.charAt(0)!=str.charAt(str.length()-1)) return false;
+        return checkPalindrome(str.substring(1,str.length()-1));
     }
     public static void main(String[] args) {
-        String str = "jgjgj";
-        int i = 0;
-        boolean result = checkPalindrome(str,i);
+        String str = "natin";
+        boolean result = checkPalindrome(str);
         System.out.println(result);
     }
 }
+
