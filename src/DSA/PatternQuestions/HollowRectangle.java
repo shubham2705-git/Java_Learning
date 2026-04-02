4a464583011ea5984f1f2fac828b613b7d696465
Shubham Ranjan <gkr74226@gmail.com>
2026-04-02 16:26:38 +0530

    commit..

---
+++ src/DSA/PatternQuestions/HollowRectangle.java
@@ -0,0 +1,19 @@
+package PatternQuestions;
+
+public class HollowRectangle {
+    public static void main(String[] args) {
+        int n=5;
+        int m=8;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
