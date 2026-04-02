1ddeea018e1e355fddbe4a47595dcdda12d2964f
Shubham Ranjan <gkr74226@gmail.com>
2026-04-02 16:13:50 +0530

    commit..

---
+++ src/DSA/PatternQuestions/Rectangle.java
@@ -0,0 +1,14 @@
+package PatternQuestions;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        int n=5;
+        int m=8;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+    }
+}
