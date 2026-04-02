7958f9ba7cb09bdf9c6af7760a72304e910fbf70
Shubham Ranjan <gkr74226@gmail.com>
2026-04-02 16:53:15 +0530

    commit..

---
+++ src/DSA/PatternQuestions/Pyramid.java
@@ -0,0 +1,17 @@
+package PatternQuestions;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<n;i++){
+
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<(2*i-1);k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
