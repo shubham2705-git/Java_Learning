70b9e46ce37c32513e303f1adb97cebc146d918f
Shubham Ranjan <gkr74226@gmail.com>
2026-04-29 02:34:31 +0530

    commit..

---
+++ src/DSA/HashMap/CountEle.java
@@ -0,0 +1,18 @@
+package HashMap;
+
+
+import java.util.*;
+
+public class CountEle {
+    public static void find(int n){
+        if((n&1) ==1){
+            System.out.println("odd");
+        }
+        if((n&1)==0){
+            System.out.println("even");
+        }
+    }
+    public static void main(String[] args) {
+        find(5);
+        }
+    }
