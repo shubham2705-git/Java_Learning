220204aae43dadd180c382fe41975f0ce518c420
Shubham Ranjan <gkr74226@gmail.com>
2026-04-30 14:16:43 +0530

    commit..

---
+++ src/DSA/HashMap/IntToRoman.java
@@ -0,0 +1,31 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IntToRoman {
+    public static void convert(int num) {
+        Map<Character, Integer> map = new HashMap<>();
+        int[] val =   {1000, 900, 500, 400,  100, 90,   50,  40,   10,   9,    5,  4,    1};
+        String[] sys ={"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder result = new StringBuilder();
+
+        for(int i=0;i<val.length;i++){
+            if(num==0) break;
+
+            int times = num/val[i];
+            while(times>0)
+            {
+                result.append(sys[i]);
+                times--;
+            }
+            num=num%val[i];
+        }
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        int n  = 1997;
+        convert(n);
+    }
+}
