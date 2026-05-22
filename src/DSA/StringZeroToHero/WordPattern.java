d93c39f0f13aac2f83e051e08758aca69b06d824
Shubham Ranjan <gkr74226@gmail.com>
2026-05-22 06:23:30 +0530

    commit..

--- src/DSA/StringZeroToHero/WordPattern.java
+++ src/DSA/StringZeroToHero/WordPattern.java
@@ -7,10 +7,12 @@ public class WordPattern {
     public static boolean wordPattern(String pattern,String s){
         String[] arr=s.split(" ");
         if(pattern.length()!=arr.length) return false;
+
         Map<Character,String> map = new HashMap<>();
         for(int i=0;i<pattern.length();i++){
             char c = pattern.charAt(i);
             String word=arr[i];
+
             if(map.containsValue(word) && !map.containsKey(c)){
                 return false;
             }
