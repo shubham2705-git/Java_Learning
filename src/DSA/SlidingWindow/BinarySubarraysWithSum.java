de64879607088d650be57dd7b6ead51bdff92e2c
Shubham Ranjan <gkr74226@gmail.com>
2026-05-26 02:35:57 +0530

    commit..

---
+++ src/DSA/SlidingWindow/BinarySubarraysWithSum.java
@@ -0,0 +1,26 @@
+package SlidingWindow;
+
+public class BinarySubarraysWithSum {
+    public static int find(int[] nums,int goal){
+        int l=0, r=0, sum=0, count=0;
+        if(goal < 0) return 0;
+        while(r < nums.length){
+            sum=sum+nums[r];
+            while(sum > goal){
+                sum = sum - nums[l];
+                l++;
+            }
+            count = count + (r-l+1);
+            r++;
+        }
+        return count;
+    }
+    public static int numSubarraysWithSum(int[] nums, int goal) {
+        return find(nums,goal) - find(nums,goal-1);
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,0,1,0,1};
+        int goal = 2;
+        System.out.println(numSubarraysWithSum(nums, goal));
+    }
+}
