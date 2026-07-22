2f99b738a97a419e36ea4683d9e58a537517c7c0
Shubham Ranjan <gkr74226@gmail.com>
2026-07-22 18:57:40 +0530

    commit..

--- src/DSA/ArrayEasyLevel/ArmStrongNumbers.java
+++ src/DSA/ArrayEasyLevel/ArmStrongNumbers.java
@@ -2,23 +2,31 @@ package ArrayEasyLevel;
 
 public class ArmStrongNumbers {
     public static void armStrong(int n){
-        int cube =0;
-        for(int i=1;i<n;i++){
+        for(int i=100;i<n;i++){
             int temp = i;
-            int sum = 0;
-            while(temp!=0){
-                int remain = temp%10;
-                cube = remain * remain * remain;
-                sum+=cube;
-                temp=temp/10;
+            int count = 0;
+            int result = 0;
+            while(temp>0){
+                count++;
+                temp = temp/10;
             }
-            if(sum==i){
+            temp = i;
+            while(temp>0){
+                int digit = temp%10;
+                int power = 1;
+                for(int j=0;j<count;j++){
+                    power = power * digit;
+                }
+                result = result + power;
+                temp  = temp/10;
+            }
+            if(result==i){
                 System.out.println(i);
             }
         }
     }
     public static void main(String[] args) {
-        int n = 1000;
+        int n = 10000;
         armStrong(n);
     }
 }
