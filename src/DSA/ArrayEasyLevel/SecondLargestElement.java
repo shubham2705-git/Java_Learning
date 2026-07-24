d386b7c0e2caca4b51bccc5f6a784c7a6a1f13b4
Shubham Ranjan <gkr74226@gmail.com>
2026-07-25 00:45:38 +0530

    commit..

--- src/DSA/ArrayEasyLevel/SecondLargestElement.java
+++ src/DSA/ArrayEasyLevel/SecondLargestElement.java
@@ -38,9 +38,9 @@ public class SecondLargestElement {
         return second;   */
 // ------------------------------------------
         // =========== Optimal Solution ======================
-        int largest = arr[0];
+        int largest = Integer.MIN_VALUE;
         int second = Integer.MIN_VALUE;
-        for(int i = 1; i<n; i++)
+        for(int i = 0; i<n; i++)
         {
             if(arr[i]>largest)
             {
@@ -54,7 +54,7 @@ public class SecondLargestElement {
         return second;
     }
     public static void main(String[] args) {
-        int arr[] = {21,34,-34,5,7,35,-39,25,-65,-65,65,-65};
+        int arr[] = {21,74,-34,5,7,35,-39,25,-65,-65,65,-65};
         int n = arr.length-1;
         int result = SecondLargest(arr,n);
         System.out.println(result);
