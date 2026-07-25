e1079fc0d606381fa0a53b43ef9ba6f4fa9dcb3e
Shubham Ranjan <gkr74226@gmail.com>
2026-07-26 01:05:02 +0530

    commit..

---
+++ src/DSA/BinarySearch/FindMinInRotatedSortedArray.java
@@ -0,0 +1,26 @@
+package BinarySearch;
+
+public class FindMinInRotatedSortedArray {
+    public static int find(int[] arr){
+        int l = 0;
+        int r = arr.length-1;
+        int min = Integer.MAX_VALUE;
+        int mid = 0;
+        while(l <= r){
+            mid = l + (r-l)/2;
+            if(arr[l] <= arr[mid]){
+                min = Math.min(arr[l], min);
+                l = mid+1;
+            }
+            else{
+                min = Math.min(arr[mid], min);
+                r = mid-1;
+            }
+        }
+        return min;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4,5,1,2,3};
+        System.out.println(find(arr));
+    }
+}
