1c8ba97a84fef0c237b1a90385a8e98d736a8753
Shubham Ranjan <gkr74226@gmail.com>
2026-07-26 15:52:17 +0530

    commit..

---
+++ src/DSA/BinarySearch/FindPeakElementIndex.java
@@ -0,0 +1,23 @@
+package BinarySearch;
+
+public class FindPeakElementIndex {
+    public static int find(int[] arr){
+        int n = arr.length;
+        if(n == 1) return 0;
+        if(arr[0] > arr[1]) return 0;
+        if(arr[n-1] > arr[n-2]) return n-1;
+        int low = 1;
+        int high = n-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]) return mid;
+            else if(arr[mid] > arr[mid-1]) low = mid +1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,1,3,5,6,4};
+        System.out.println(find(arr));
+    }
+}
