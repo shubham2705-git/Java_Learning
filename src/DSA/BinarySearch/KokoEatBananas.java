e0f7614da21d2544ec79620b5ab69a5a77afe993
Shubham Ranjan <gkr74226@gmail.com>
2026-07-28 03:08:51 +0530

    Koko Eat Bananas..

---
+++ src/DSA/BinarySearch/KokoEatBananas.java
@@ -0,0 +1,38 @@
+package BinarySearch;
+
+import java.util.Arrays;
+
+public class KokoEatBananas {
+    private static int calculateTotalHours(int[] piles, int speed) {
+        int totalH = 0;
+        for (int bananas : piles) {
+            totalH += (int)Math.ceil((double)bananas / speed);
+        }
+        return totalH;
+    }
+    public static int find(int[] arr, int h){
+        int maxPile = Arrays.stream(arr).max().getAsInt();
+        int low = 1;
+        int high = maxPile;
+        int ans = maxPile;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            int totalH = calculateTotalHours(arr, mid);
+
+            if (totalH <= h) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {30,11,23,4,20};
+        int h = 5;
+        System.out.println(find(arr,h));
+    }
+}
