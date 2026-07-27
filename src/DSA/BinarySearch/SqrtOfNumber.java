e353a7cdf8429e2707f0cb1c9271c8d09d59c838
Shubham Ranjan <gkr74226@gmail.com>
2026-07-28 01:10:42 +0530

    Square root of a number..

---
+++ src/DSA/BinarySearch/SqrtOfNumber.java
@@ -0,0 +1,24 @@
+package BinarySearch;
+
+public class SqrtOfNumber {
+    public static int find(int n){
+        int low = 1;
+        int high = n;
+        int result = 0;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if((mid * mid) <= n){
+                result = mid;
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(find(n));
+    }
+}
