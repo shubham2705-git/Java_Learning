d091105f144d2379bacbfb80fb8fb68016293a8d
Shubham Ranjan <gkr74226@gmail.com>
2026-08-04 10:22:51 +0530

    Math.abs..

---
+++ src/DSA/BasicMaths/AbsFunction.java
@@ -0,0 +1,10 @@
+package BasicMaths;
+
+public class AbsFunction {
+    public static void main(String[] args) {
+        int n = -5;
+        int m = Math.abs(n);
+        System.out.println(n);
+        System.out.println(m);
+    }
+}
