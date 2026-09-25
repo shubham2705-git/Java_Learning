316453264558bbf28dd64218149cdd49e06056bd
Shubham Ranjan <gkr74226@gmail.com>
2026-09-25 21:41:09 +0530

    commit..

--- src/DSA/PatternQuestions/pattern10.java
+++ src/DSA/PatternQuestions/pattern10.java
@@ -3,7 +3,6 @@ package PatternQuestions;
 public class pattern10 {
     public static void main(String[] args) {
         int n = 5;
-
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= i; j++){
                 System.out.print(i+" ");
