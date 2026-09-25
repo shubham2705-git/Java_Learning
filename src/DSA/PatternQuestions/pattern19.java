d4cd3238f1c4c1342330a4ed20f0f4c4eb75d23c
Shubham Ranjan <gkr74226@gmail.com>
2026-09-25 18:29:54 +0530

    commit..

---
+++ src/DSA/PatternQuestions/pattern19.java
@@ -0,0 +1,22 @@
+package PatternQuestions;
+
+public class pattern19 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+
+            for(int j = 1; j <= n-i; j++){
+                System.out.print("  ");
+            }
+
+            for(int j = 1; j <= i; j++){
+                if(i % 2 != 0)
+                    System.out.print((char)(64 + j)+" ");
+                else
+                    System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
