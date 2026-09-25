5fe3ab6468b3c56c27187215493d8205bf9cecd0
Shubham Ranjan <gkr74226@gmail.com>
2026-09-25 18:04:18 +0530

    commit..

---
+++ src/DSA/PatternQuestions/pattern14.java
@@ -0,0 +1,16 @@
+package PatternQuestions;
+
+public class pattern14 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+                if(i%2!=0)
+                    System.out.print((char)(j+64)+" ");
+                else
+                    System.out.print((char)(j+96)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
