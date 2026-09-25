edbf850088a79eddc2f5a14e5aa16b9462306d2a
Shubham Ranjan <gkr74226@gmail.com>
2026-09-25 18:20:17 +0530

    commit..

---
+++ src/DSA/PatternQuestions/pattern18.java
@@ -0,0 +1,16 @@
+package PatternQuestions;
+
+public class pattern18 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=5; j++){
+                if(j<=(n-i))
+                    System.out.print("  ");
+                else
+                    System.out.print((char)(96+i)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
